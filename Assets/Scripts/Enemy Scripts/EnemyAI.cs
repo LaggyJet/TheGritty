cc35ee715601ebe19f6492b5d43d08c1a7b6eb32
PJ Glover <pjglover11@gmail.com>
2024-06-11 15:05:40 -0400

    Spawn Point prefab and basic spawnpoint script

--- Assets/Scripts/Enemy Scripts/EnemyAI.cs
+++ Assets/Scripts/Enemy Scripts/EnemyAI.cs
@@ -1,4 +1,4 @@
-//worked on by - Joshua Furber, natalie lubahn
+//worked on by - Joshua Furber, natalie lubahn, PJ Glover
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
