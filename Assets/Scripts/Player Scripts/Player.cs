62f1ba9cd3076ac36f3f42d65ee4dbe317c3399e
jiirvin <147550496+jiirvin@users.noreply.github.com>
2024-06-13 04:06:04 -0400

    Bug Fixing

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -137,6 +137,7 @@ public class PlayerController : MonoBehaviour, IDamage
         //spawns our projectile
         else
         {
+            yield return new WaitForSeconds(.2f);
             Instantiate(projectile, shootPosition.transform.position, shootPosition.transform.rotation);
         }
         
