8e259c494e5f13123cf87b4f75bba52655da6933
jiirvin <147550496+jiirvin@users.noreply.github.com>
2024-07-03 11:34:34 -0400

    Started Warrior Class

--- Assets/Scripts/Player Scripts/Class_Warrior.cs
+++ Assets/Scripts/Player Scripts/Class_Warrior.cs
@@ -1,18 +1,68 @@
+//Worked on By : Jacob Irvin, Joshua Furber
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Class_Warrior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerController player;
+    bool isAttacking;
+    float damage;
+    bool canDOT;
+    DamageStats type;
+    float swingRate;
+
+    private void Start()
     {
-        
+        player = GameManager.instance.player.GetComponent<PlayerController>();
+        player.combatObjects[3].AddComponent<MeleeWeaponController>().SetWeapon(damage, canDOT, type);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!GameManager.instance.isPaused && SceneManager.GetActiveScene().name != "title menu")
+        {
+            AttackCheck();
+        }
     }
+
+    void AttackCheck()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //plays our primary shooting animation
+            player.SetAnimationTrigger("Warrior1");
+        }
+        if (Input.GetButtonDown("Fire2") && !isAttacking)
+        {
+            isAttacking = true;
+            player.SetAnimationBool("Warrior2", true);
+        }
+        if (Input.GetButtonUp("Fire2") && isAttacking)
+        {
+            isAttacking = false;
+            player.SetAnimationBool("Warrior2", false);
+        }
+    }
+    public void WeaponColliderOn() 
+    {
+        player.combatObjects[3].GetComponent<Collider>().enabled = true; 
+    }
+    public void WeaponColliderOff() 
+    {
+        player.combatObjects[3].GetComponent<Collider>().enabled = false; 
+    }
+
+    IEnumerator Swing()
+    {
+        isAttacking = true;
+        WeaponColliderOn();
+        yield return new WaitForSeconds(swingRate);
+        WeaponColliderOff();
+        isAttacking = false;
+    }
+
+
 }
