a9be3a25c6c6b837e32fd1cec318194414be749c
KheeraKing <kmaaranu@student.fullsail.edu>
2024-07-06 01:17:05 -0400

    Sounds & Regeneration
    Added stamina regeneration without player pickups. Added sounds to pick up health and stamina.

--- Assets/Scripts/Map/HPotPickUp.cs
+++ Assets/Scripts/Map/HPotPickUp.cs
@@ -8,10 +8,16 @@ public class HPotPickUp : MonoBehaviour
     // Amount of hp/stamina the potion adds to the player
     [Range(0f, 10f)] public float hpAdd;  
     [Range(0f, 10f)] public float staminaAdd; 
+    [SerializeField] public AudioSource Audio; 
+    [SerializeField] AudioClip pickUp;
+   
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Audio.clip = pickUp; 
+            Audio.Play();
             GameManager.instance.playerScript.AddHP(hpAdd);
             GameManager.instance.playerScript.AddStamina(staminaAdd);
             Destroy(gameObject);
