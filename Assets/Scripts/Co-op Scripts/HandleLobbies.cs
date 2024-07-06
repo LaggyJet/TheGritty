80459a02bad3f9f48a3671cdca8c20457c03e76a
Joshua Furber <jskrillex6@gmail.com>
2024-07-06 18:49:11 -0400

    Some error handling with the lobbies

--- Assets/Scripts/Co-op Scripts/HandleLobbies.cs
+++ Assets/Scripts/Co-op Scripts/HandleLobbies.cs
@@ -4,9 +4,16 @@ using Photon.Realtime;
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
 public class HandleLobbies : MonoBehaviourPunCallbacks {
     [SerializeField] TMP_InputField hostInput, joinInput;
     [SerializeField] GameObject loadMenu, lobbyMenu;
+    [SerializeField] TMP_Text infoText;
+    [SerializeField] Button hostButton, joinButton;
+
+    List<RoomInfo> rooms = new();
 
     // Start the connection when loading screen plays
     void Start() { PhotonNetwork.AutomaticallySyncScene = true; PhotonNetwork.ConnectUsingSettings(); }
@@ -17,8 +24,21 @@ public class HandleLobbies : MonoBehaviourPunCallbacks {
     // Display the lobby menu
     public override void OnJoinedLobby() { loadMenu.SetActive(false); lobbyMenu.SetActive(true); }
 
+    public override void OnRoomListUpdate(List<RoomInfo> roomList) { rooms = roomList; }
+
     // Create room based on user input (max of 2 players)
-    public void HostRoom() { PhotonNetwork.CreateRoom(hostInput.text, new RoomOptions() { MaxPlayers = 2 }, null); }
+    public void HostRoom() {
+        for (int i = 0; i < rooms.Count; i++) {
+            if (rooms[i].Name == hostInput.text) {
+                infoText.color = Color.red;
+                infoText.text = "Room with this name already exists";
+                return;
+            }
+        }
+        
+        hostButton.interactable = false;
+        PhotonNetwork.CreateRoom(hostInput.text, new RoomOptions() { MaxPlayers = 2 }, null);
+    }
 
     // Join room based on user input
     public void JoinRoom() { PhotonNetwork.JoinRoom(joinInput.text, null); }
@@ -27,7 +47,9 @@ public class HandleLobbies : MonoBehaviourPunCallbacks {
     public override void OnJoinedRoom() { StartCoroutine(WaitOnLobby()); }
 
     IEnumerator WaitOnLobby() {
-        while (PhotonNetwork.CurrentRoom.PlayerCount != 2)
+        infoText.text = "Connected: Waiting for other";
+        joinInput.interactable = joinButton.interactable = false;
+        while (PhotonNetwork.CurrentRoom.PlayerCount != PhotonNetwork.CurrentRoom.MaxPlayers)
             yield return null;
         if (PhotonNetwork.IsMasterClient)
             PhotonNetwork.LoadLevel("Build Scene");
