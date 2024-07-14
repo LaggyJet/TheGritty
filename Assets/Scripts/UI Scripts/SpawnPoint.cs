4d43261582dda6554846cb613cf2e3d9a3d29354
LaggyJet <jskrillex6@gmail.com>
2024-07-13 22:16:55 -0400

    Updated spawning for multiplayer to prevent glitching

--- Assets/Scripts/UI Scripts/SpawnPoint.cs
+++ Assets/Scripts/UI Scripts/SpawnPoint.cs
@@ -11,10 +11,11 @@ public class SpawnPoint : MonoBehaviourPunCallbacks
 
         if (PhotonNetwork.InRoom && player != null)
             PhotonNetwork.Instantiate("Player/" + player.name, GetRandomSpawn(), Quaternion.identity);
-        else if (!PhotonNetwork.InRoom && player != null)
-            Instantiate(player, GetRandomSpawn(), Quaternion.identity);
-            GameManager.playerLocation = GetRandomSpawn();
+        else if (!PhotonNetwork.InRoom && player != null) {
+            Instantiate(player, gameObject.transform.position, Quaternion.identity);
+            GameManager.playerLocation = gameObject.transform.position;
+        }
     }
 
-    Vector3 GetRandomSpawn() { return gameObject.transform.position; }
+    Vector3 GetRandomSpawn() { return gameObject.transform.position + new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2)); }
 }
