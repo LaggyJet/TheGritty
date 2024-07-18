bc4d087190847928714b16288e73dcc341dad5a8
xx0i <natalie9705@gmail.com>
2024-07-18 17:03:20 -0400

    story prefabs and scripts made + implemented what i could
    waiting for more work to be done on the map to add more parts
    
    guys am i serving blokecore

---
+++ Assets/Scripts/UI Scripts/storyText.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class storyText : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    [SerializeField] string text;
+    float charactersPerSecond = 40;
+    private bool activated = false;
+ 
+    IEnumerator TypeTextUncapped(string line)
+    {
+        float timer = 0;
+        float interval = 1 / charactersPerSecond;
+        string textBuffer = null;
+        char[] chars = line.ToCharArray();
+        int i = 0;
+
+        while (i < chars.Length)
+        {
+            if (timer < Time.deltaTime)
+            {
+                textBuffer += chars[i];
+                textMeshPro.text = textBuffer;
+                timer += interval;
+                i++;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        textMeshPro.text = "";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !activated && !PhotonNetwork.IsConnected)
+        {
+            StartCoroutine(TypeTextUncapped(text));
+            activated = true;
+        }
+    }
+}
